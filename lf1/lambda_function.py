eed0f67b7ca2a82d018123fa8041e0e242e59152
saihari22 <89160461+saihari22@users.noreply.github.com>
2022-04-21 21:22:10 -0400

    Update lambda_function.py

--- lf1/lambda_function.py
+++ lf1/lambda_function.py
@@ -1,8 +1,18 @@
 import json
+import boto3
+from boto3.dynamodb.conditions import Key
 
+    
 def lambda_handler(event, context):
     # TODO implement
+    key = event['key']
+    print("K", key)
+    
+    table = boto3.resource('dynamodb').Table('events')
+    result = table.query(KeyConditionExpression=Key('id').eq("Z7r9jZ1Ad8pJf"))
+    print("R", result)
+    
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!!')
+        'body': json.dumps('Hello from Lambda!')
     }
